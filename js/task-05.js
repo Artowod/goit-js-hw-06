823e57a18b4492c56fc7ed042411954c5bf09d46
Artowod <myjavaname@gmail.com>
2021-09-23 18:49:51 +0300

    5th task - done

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+/* 
+Напиши скрипт который, при наборе текста в инпуте 
+input#name - input(событие input), подставляет его 
+текущее значение в span#name - output.
+Если инпут пустой, в спане должна отображаться строка "Anonymous".
+
+<input type="text" id="name-input" placeholder="Please enter your name" />
+<h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+ */
+
+document.addEventListener('keyup', event => {
+  const inputItem = document.querySelector('#name-input');
+  const outputItem = document.querySelector('#name-output');
+  if (inputItem.value === '') {
+    outputItem.textContent = 'Anonymous';
+  } else {
+    outputItem.textContent = inputItem.value;
+  }
+});
