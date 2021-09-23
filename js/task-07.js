5b69ec961c0aa95765ccdb778d76772540150926
Artowod <myjavaname@gmail.com>
2021-09-23 23:23:52 +0300

    7 task - done

--- js/task-07.js
+++ js/task-07.js
@@ -1,19 +1,24 @@
-
-
-
-
 /* 
-const handleTextInputClick = event => {
-    console.log('a');
+Напиши скрипт, который реагирует на изменение значения 
+input#font-size-control (событие input) и изменяет инлайн-стиль 
+span#text обновляя свойство font-size. 
+В результате при перетаскивании ползунка будет меняться размер текста.
 
- }
-textInput.addEventListener('click', handleTextInputClick);
- */
+<input id="font-size-control" type="range" min="16" max="96" />
+<br />
+<span id="text">Abracadabra!</span> 
+*/
 
-/* textInput.addEventListener('focus', event => {
-  console.log('OPAA');
-}); */
+const inputRangeElement = document.querySelector('#font-size-control');
+const textElement = document.querySelector('#text');
 
-/* textInput.addEventListener('blur', event => {
-    console.log('HAHAHAHA');
-}); */
+/* const handleRangeElement = event => {
+
+} */
+
+function handleRangeElement(event) {
+  textElement.style.fontSize = `${inputRangeElement.value}px`;
+  console.log(`${inputRangeElement.value}px`);
+}
+
+inputRangeElement.addEventListener('input', handleRangeElement);
