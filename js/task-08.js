52c29d9efb73668caae55497446ed3dab988c80c
Artowod <myjavaname@gmail.com>
2021-09-24 16:51:00 +0300

    task08 - done

--- js/task-08.js
+++ js/task-08.js
@@ -12,27 +12,33 @@
   <button type="submit">Login</button>
 </form>
 
-Обработка отправки формы form.login-form должна быть по событию submit.
-При отправке формы страница не должна перезагружаться.
-Если в форме есть незаполненные поля, выводи alert с 
++Обработка отправки формы form.login-form должна быть по событию submit.
++При отправке формы страница не должна перезагружаться.
++Если в форме есть незаполненные поля, выводи alert с 
 предупреждением о том, что все поля должны быть заполнены.
-Если пользователь заполнил все поля и отправил форму, собери 
++Если пользователь заполнил все поля и отправил форму, собери 
 значения полей в обьект, где имя поля будет именем свойства, 
-а значение поля - значением свойства. Для доступа к элементам формы 
-используй свойство elements.
-Выведи обьект с введенными данными в консоль и очисти значения полей 
+а значение поля - значением свойства. 
++Для доступа к элементам формы используй свойство elements.
++Выведи обьект с введенными данными в консоль и очисти значения полей 
 формы методом reset.
 */
 
-const form = document.querySelector(".login-form");
+const form = document.querySelector('.login-form');
 
 function handleForm(event) {
-    event.preventDefault();
-    console.log(form.elements['email'].value);
-    console.log(form.elements['password'].value); 
+  event.preventDefault();
 
-    console.log(event.currentTarget.elements['email'].value);
-    console.log(event.currentTarget.elements['password'].value); 
+  const formData = {
+    email: event.currentTarget.elements['email'].value,
+    password: event.currentTarget.elements['password'].value,
+  };
 
+  if (formData.email === '' || formData.password === '') alert('All fields must be filled in!');
+  else {
+    console.log(formData);
+    form.reset();
+  }
 }
+
 form.addEventListener('submit', handleForm);
